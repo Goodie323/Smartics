6105288e51bb70b5827fcbd2b37f3e8ef050c162
unknown <awoleyegoodness5@gmail.com>
2025-07-30 15:01:05 +0100

    Fix import path to app.api.routes

--- Backendd/app/main.py
+++ Backendd/app/main.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
-from api.routes import bp  # Import the Blueprint
+from app.api.routes import bp  # Import the Blueprint
 
 app = Flask(__name__)
 CORS(app)
