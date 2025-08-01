844f224f19f89bfab09f08136aa6e2c3b60d8496
unknown <awoleyegoodness5@gmail.com>
2025-08-01 05:28:26 +0100

    commit latest edit on main

--- Backendd/app/main.py
+++ Backendd/app/main.py
@@ -1,15 +1,11 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 from app.api.routes import bp  # Import the Blueprint
-from app.database import create_table_if_not_exists  # Optional: create table on startup
+
 
 app = Flask(__name__)
 CORS(app)
 
-# Optional: ensure DB table exists before first request
-@app.before_first_request
-def init_db():
-    create_table_if_not_exists()
 
 # Register blueprint with URL prefix
 app.register_blueprint(bp, url_prefix='/api')
